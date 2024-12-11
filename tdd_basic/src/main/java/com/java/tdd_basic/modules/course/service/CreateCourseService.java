56da1750a4457eb45f4cca65178761b3614b3f82
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-11 08:36:01 -0300

    wip: add CouseService
    Method returns null

---
+++ tdd_basic/src/main/java/com/java/tdd_basic/modules/course/service/CreateCourseService.java
@@ -0,0 +1,10 @@
+package com.java.tdd_basic.modules.course.service;
+
+import com.java.tdd_basic.modules.course.entity.Course;
+
+public class CreateCourseService {
+    public Course execute(Course course) {
+        return null;
+    }
+
+}
