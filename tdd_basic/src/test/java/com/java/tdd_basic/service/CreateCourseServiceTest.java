ff0253e06cef4704724caf42cb3d7745badc10e0
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-14 17:09:43 -0300

    test: add test in case course exists

--- tdd_basic/src/test/java/com/java/tdd_basic/service/CreateCourseServiceTest.java
+++ tdd_basic/src/test/java/com/java/tdd_basic/service/CreateCourseServiceTest.java
@@ -1,25 +1,37 @@
 package com.java.tdd_basic.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import com.java.tdd_basic.modules.course.repository.CourseInMemoryRepository;
 import org.junit.jupiter.api.Test;
 
 import com.java.tdd_basic.modules.course.entity.Course;
 import com.java.tdd_basic.modules.course.service.CourseService;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CreateCourseServiceTest {
 
-	@Test
-	public void should_be_able_to_create_a_new_course() {
-		Course course = new Course();
-		course.setDescription("Course_Description_Test");
-		course.setName("Course_Name");
-		course.setWorkload(100);
-		CourseInMemoryRepository repository = new CourseInMemoryRepository();
-		CourseService createCourseService = new CourseService(repository);
-		Course createdCourse = createCourseService.execute(course);
-		assertNotNull(createdCourse.getId());
-	}
+    @Test
+    public void should_be_able_to_create_a_new_course() {
+        Course course = new Course();
+        course.setDescription("Course_Description_Test");
+        course.setName("Course_Name");
+        course.setWorkload(100);
+        CourseInMemoryRepository repository = new CourseInMemoryRepository();
+        CourseService createCourseService = new CourseService(repository);
+        Course createdCourse = createCourseService.execute(course);
+        assertNotNull(createdCourse.getId());
+    }
 
+    @Test()
+    public void should_not_be_able_to_create_a_new_course_if_exists() {
+        Course course = new Course();
+        course.setDescription("Not_Create_course");
+        course.setName("Not_Create_course");
+        course.setWorkload(100);
+        CourseInMemoryRepository repository = new CourseInMemoryRepository();
+        CourseService createCourseService = new CourseService(repository);
+        createCourseService.execute(course);
+        Error error = assertThrows(Error.class, () -> createCourseService.execute(course));
+        assertTrue(error.getMessage().contains("Course exists"));
+    }
 }
